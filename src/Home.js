429500324218675ba35652f59441db266e21403a
Zui <zuikng@gmail.com>
2017-11-06 23:33:27 -0500

    Begin porting over Navbar
    * Add css for Homepage

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+
+}
+
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+/*
+This is the navbar code in HTML
+
+<header class="container-fluid">
+    <div class="wrapper" id="navbar">
+        <div id="dummy-div"></div>
+        <div id="menu-section">
+            <ul class="nav navigation" id="menu">
+                <li class="nav-item"><a href="#about">ABOUT</a></li>
+                <li class="nav-item"><a href="#news">NEWS</a></li>
+                <li class="nav-item"><a href="#members">MEMBERS</a></li>
+                <li class="nav-item"><a href="#teams">TEAMS</a></li>
+                <li class="nav-item"><a href="#sponsors">SPONSORS</a></li>
+            </ul>
+        </div>
+        <div id="social_section">
+            <div id="fb-icon">
+                <a href="https://www.facebook.com/groups/1017831178363028/?ref=bookmarks">
+                    <img src="image/icons/fb.png">
+                </a>
+            </div>
+            <div id="github-icon">
+                <a href="https://github.com/bumic">
+                    <img src="image/icons/github.png">
+                </a>
+            </div>
+            <div id="mail-icon">
+                <a href="mailto:bumic@bu.edu">
+                    <img src="image/icons/mail.png">
+                </a>
+            </div>
+            <div id="medium-icon">
+                <a href="https://medium.com/@bumic_">
+                    <img src="image/icons/medium.png">
+                </a>
+            </div>
+            <div id="twitter-icon">
+                <a href="https://twitter.com/@bumic_">
+                    <img src="image/icons/twitter.png">
+                </a>
+            </div>
+        </div>
+    </div>
+</header>
+
+ */
